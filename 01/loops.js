574040a0961ae5d2cb4502aed7e28e68dd73e19c
Pavel Schoffer <xschof00@gmail.com>
2019-10-15 11:34:10 +0200

    remove sollution

--- 01/loops.js
+++ 01/loops.js
@@ -1,29 +1,3 @@
-// While (numbers till 5+)
-
-// console.log(1);
-// console.log(2);
-// console.log(3);
-// console.log(4);
-// console.log(5);
-
-let number = 1;
-while (number <= 100) {
-  console.log(number);
-  number++;
-}
+// While (print numbers till 5+)
 
 // For (sum of items)
-
-let items = [
-  { name: "book", qt: 2 },
-  { name: "pen", qt: 23 },
-  { name: "block", qt: 7 },
-  { name: "apples", qt: 16 }
-];
-
-let sum = 0;
-for (let index = 0; index < items.length; index++) {
-  sum += items[index].qt;
-}
-
-console.log("Sum is: " + sum);
